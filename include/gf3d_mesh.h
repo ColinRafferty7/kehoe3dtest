a977e76aef99faec671d27b0a9a86d1e7a824317
ColinRafferty7 <ctr6mince@gmail.com>
2025-10-29 20:04:59 -0400

    Saving branch

--- include/gf3d_mesh.h
+++ include/gf3d_mesh.h
@@ -224,6 +224,7 @@ void gf3d_mesh_setup_face_buffers(MeshPrimitive* mesh, Face* faces, Uint32 fcoun
 //TODO
 void gf3d_mesh_queue_render(Mesh* mesh, Pipeline* pipe, void* uboData, Texture* texture);
 
+//TODO
 Pipeline *gf3d_mesh_get_sky_pipeline();
 
 #endif
