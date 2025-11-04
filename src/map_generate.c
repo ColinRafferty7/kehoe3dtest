a3b8cc66be4651cd709b589678a8654adf36d1bb
ColinRafferty7 <ctr6mince@gmail.com>
2025-11-04 13:55:25 -0500

    Random level generation complete with slope orientation

--- src/map_generate.c
+++ src/map_generate.c
@@ -2,12 +2,13 @@
 
 #define SCALE 32
 #define SIZE 8
-#define HEIGHT 3
-#define SLOPE_CHANCE 5
+#define HEIGHT 2
+#define SLOPES 4
 
 void map_generate_level()
 {
 	// TODO: Entity update order is not good
+	// TODO: Optimize and restructure the code. This demonstrates horrible coding practices
 
 	int map_height[SIZE * 2][SIZE * 2];
 
@@ -39,11 +40,11 @@ void map_generate_level()
 	{
 		for (int j = (SIZE * -1); j < SIZE; j++)
 		{
-			int chance = gfc_random_int(SLOPE_CHANCE);
+			int chance = gfc_random_int(SLOPES);
 
 			if (chance == 0)
 			{
-				if (fabs(map_height[i][j - 1] - map_height[i][j + 1]) == 1 && map_height[i][j] != HEIGHT - 1)
+				if (map_height[i + 1][j] - map_height[i - 1][j] == 1 && map_height[i][j] != HEIGHT - 1)
 				{
 					Entity* slope;
 					slope = entity_new();
@@ -51,6 +52,57 @@ void map_generate_level()
 					slope->isSlope = 1;
 					slope->name = "Slope Block";
 					slope = gf3d_model_load(slope, "models/slope.model");
+
+					gfc_vector3d_scale(slope->scale, slope->scale, SCALE);
+					gfc_vector3d_add(slope->position, slope->position, gfc_vector3d((2 * SCALE * i), (2 * SCALE * j), ((map_height[i][j] + 1) * 2 * SCALE) - 150));
+				}
+			}
+			if (chance == 1)
+			{
+				if (map_height[i - 1][j] - map_height[i + 1][j] == 1 && map_height[i][j] != HEIGHT - 1)
+				{
+					Entity* slope;
+					slope = entity_new();
+					slope->isStatic = 1;
+					slope->isSlope = 1;
+					slope->name = "Slope Block";
+					slope = gf3d_model_load(slope, "models/slope.model");
+
+					slope->rotation.z = GFC_PI;
+
+					gfc_vector3d_scale(slope->scale, slope->scale, SCALE);
+					gfc_vector3d_add(slope->position, slope->position, gfc_vector3d((2 * SCALE * i), (2 * SCALE * j), ((map_height[i][j] + 1) * 2 * SCALE) - 150));
+				}
+			}
+			if (chance == 1)
+			{
+				if (map_height[i][j + 1] - map_height[i][j - 1] == 1 && map_height[i][j] != HEIGHT - 1)
+				{
+					Entity* slope;
+					slope = entity_new();
+					slope->isStatic = 1;
+					slope->isSlope = 1;
+					slope->name = "Slope Block";
+					slope = gf3d_model_load(slope, "models/slope.model");
+
+					slope->rotation.z = GFC_HALF_PI;
+
+					gfc_vector3d_scale(slope->scale, slope->scale, SCALE);
+					gfc_vector3d_add(slope->position, slope->position, gfc_vector3d((2 * SCALE * i), (2 * SCALE * j), ((map_height[i][j] + 1) * 2 * SCALE) - 150));
+				}
+			}
+			if (chance == 3)
+			{
+				if (map_height[i][j - 1] - map_height[i][j + 1] == 1 && map_height[i][j] != HEIGHT - 1)
+				{
+					Entity* slope;
+					slope = entity_new();
+					slope->isStatic = 1;
+					slope->isSlope = 1;
+					slope->name = "Slope Block";
+					slope = gf3d_model_load(slope, "models/slope.model");
+
+					slope->rotation.z = GFC_PI_HALFPI;
 
 					gfc_vector3d_scale(slope->scale, slope->scale, SCALE);
 					gfc_vector3d_add(slope->position, slope->position, gfc_vector3d((2 * SCALE * i), (2 * SCALE * j), ((map_height[i][j] + 1) * 2 * SCALE) - 150));
